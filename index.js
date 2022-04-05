74a567657f2ac5227c20930d9f338e3a83f3c8f9
Gabriel <gabriel.braz@hotmail.com>
2022-04-05 15:42:39 -0300

    Express adicionado.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-console.log("Oi Gente!");
+console.log("Iniciando Meu Servidor...")
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/teste', (req, res) => {
+    res.send('Você Acessou a Rota /teste!')
+  })
+
+app.listen(port, () => {
+  console.log(`Servidor escutando na ${port}`)
+})
