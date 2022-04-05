49b51afb9c2eac791f1b1018376f5ee4ed9e95f0
Gabriel <gabriel.braz@hotmail.com>
2022-04-05 16:17:41 -0300

    '

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/teste', (req, res) => {
-    res.send('Você Acessou a Rota /teste!')
-  })
+app.get('/bandas', (req, res) => {
+    console.log('Alguém Acessou a Rota /bandas');
+    console.log(req.params)
+    res.json({
+        "1": {
+            "id": 1
+            "nome": "banda"
+        }
+})
+})
 
 app.listen(port, () => {
   console.log(`Servidor escutando na ${port}`)
